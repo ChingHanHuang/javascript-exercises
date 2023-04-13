139720448cba2ba7225293c0c82d38c59d662e56
Ching Han <120117415+ChingHanHuang@users.noreply.github.com>
2023-04-13 15:56:09 +0800

    Split string into character array and add each character to a created variable reversely

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const myArray = text.split("");
+    let reverseText = "";
+    for (let character in myArray) {
+        reverseText = myArray[character] + reverseText;
+    }
+    return reverseText;
 };
 
 // Do not edit below this line
