e8b067fb35e0a4768955ea5932dbeada2505fa74
Ching Han <120117415+ChingHanHuang@users.noreply.github.com>
2023-04-13 17:05:52 +0800

    Find the sum of all natural numbers in range small number to large number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (typeof(min) !== "number" || typeof(max) !== "number" || min < 0 || max < 0)
+        return "ERROR";
+    return min < max ? sumInRange(min, max) : sumInRange(max, min);
 };
+
+const sumNatural = function(n) {
+    return (n + 1) * n / 2;
+}
+
+const sumInRange = function(l, r) {
+    return sumNatural(r) - sumNatural(l - 1);
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
