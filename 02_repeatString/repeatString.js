fc340e142e396b9096e013962911e9ca18339c73
Ching Han <120117415+ChingHanHuang@users.noreply.github.com>
2023-04-13 15:43:26 +0800

    Create a variable to hold the repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0)
+        return "ERROR";
+    let ans = "";
+    while(times > 0) {
+        ans += word;
+        times--;
+    }
+    return ans;
 };
 
 // Do not edit below this line
