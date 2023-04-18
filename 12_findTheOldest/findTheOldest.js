14d6a95b04ffa9343ce0cad99756b96c9307154b
Ching Han <120117415+ChingHanHuang@users.noreply.github.com>
2023-04-18 15:52:05 +0800

    Write a function that returns the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = getAge(
+      oldestPerson.yearOfBirth,
+      oldestPerson.yearOfDeath
+    );
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge > currentAge ? oldestPerson : currentPerson;
+  });
+};
 
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
