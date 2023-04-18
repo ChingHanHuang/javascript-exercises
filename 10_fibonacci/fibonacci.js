96fe9289a6a9131a6793e32b65f55a65442aa13b
Ching Han <120117415+ChingHanHuang@users.noreply.github.com>
2023-04-18 14:47:57 +0800

    Write a function that returns a specific member of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (givenNum) {
+  if (givenNum <= 0) return "OOPS";
+  let firstNum = 1;
+  let secondNum = 1;
+  while (givenNum > 2) {
+    let temp = firstNum;
+    firstNum = secondNum;
+    secondNum += temp;
+    givenNum--;
+  }
+  return secondNum;
 };
 
 // Do not edit below this line
